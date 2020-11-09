9fd4cb1b2e61bb29b0585204c49e4e1870d2122c
NgynAnhTai <72752092+NgynAnhTai@users.noreply.github.com>
2020-11-09 10:06:34 +0700

    cart

--- js/Add to Cart.js
+++ js/Add to Cart.js
@@ -17,14 +17,14 @@ var shoppingCart = (function () {
 
     // Save cart
     function saveCart() {
-        sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
+        localStorage.setItem("shoppingCart", JSON.stringify(cart));
     }
 
     // Load cart
     function loadCart() {
-        cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
+        cart = JSON.parse(localStorage.getItem("shoppingCart"));
     }
-    if (sessionStorage.getItem("shoppingCart") != null) {
+    if (localStorage.getItem("shoppingCart") != null) {
         loadCart();
     }
 
